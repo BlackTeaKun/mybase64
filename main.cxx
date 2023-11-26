28d835878b7675eeb146f3a6186c587820bf403a
Zirui <zhang-zr@foxmail.com>
2023-11-26 22:57:53 +0800

    Add unpadded decode.

--- main.cxx
+++ main.cxx
@@ -9,14 +9,20 @@ constexpr char smallStart   = 0x1a;
 constexpr char numberStart  = 0x34;
 
 union int32char{
-  uint32_t dataInt;
-  char dataChar[4];
+  uint32_t      dataInt;
+  unsigned char dataChar[4];
 };
 
 char charTable(char in){
   if(in >= numberStart)  return '0' + (in - numberStart);
   if(in >= smallStart)   return 'a' + (in - smallStart);
   return 'A' + (in - capitalStart);
+}
+
+uint32_t binTable(char in){
+  if(std::isupper(in)) return capitalStart + in - 'A';
+  if(std::islower(in)) return smallStart + in - 'a';
+  return numberStart + in - '0';
 }
 
 void encode(const char *in, char *out){
@@ -34,47 +40,98 @@ void encodePadded(const char *in, char *out, int sz){
   int32char tmp;
   tmp.dataInt = 0;
   for(int i = 0; i < sz; ++i) tmp.dataChar[2-i] = in[i];
-  int nout = (sz*8+5)/6;
+  int nOut = (sz*8+5)/6;
   for(int i = 0; i < 4; ++i){
     out[3-i] = charTable(tmp.dataInt&0x3F);
     tmp.dataInt >>= 6;
   }
-  for(int i = nout; i < 4; ++i){
+  for(int i = nOut; i < 4; ++i){
     out[i] = '=';
   }
 }
 
-int main(int argc, char **argv){
+void decode(const char *in, char *out){
+  int32char tmp;
+  tmp.dataInt = 0;
+  for(int i = 0; i < 4; ++i){
+    tmp.dataInt += binTable(in[i]) << ((3-i)*6);
+  }
+  out[0] = tmp.dataChar[2];
+  out[1] = tmp.dataChar[1];
+  out[2] = tmp.dataChar[0];
+}
+
+std::vector<char> readData(const std::string &path, bool doEncode){
   std::ifstream in;
+  in.open(path, std::ios::binary | std::ios::in | std::ios::ate);
+
+  size_t size = in.tellg();
+  std::vector<char> data(size);
+
+  in.seekg(0);
+  if(doEncode){
+    in.read(data.data(), size);
+  }
+  else{
+    size_t i = 0;
+    while(true){
+      char chr = in.get();
+      if(in.eof()) break;
+      if(chr == '\n') continue;
+
+      data[i] = chr;
+      ++i;
+    }
+    data.resize(i);
+  }
+  return data;
+}
+
+void writeData(std::ostream &out, const std::vector<char> &outData, bool doEncode){
+  for(int i = 0; i < outData.size(); ++i){
+    out.put(outData[i]);
+    if(doEncode){
+      if((i+1) % 76 == 0){
+        out.put('\n');
+      }
+    }
+  }
+  out << std::endl;
+}
+
+int main(int argc, char **argv){
   if(argc == 1){
     return 0;
   }
-  in.open(argv[1], std::ios::binary | std::ios::in | std::ios::ate);
-  size_t size = in.tellg();
+  bool doEncode = false;
 
-  auto indata = new char[size];
-  in.seekg(0);
-  in.read(indata, size);
+  auto data = readData(argv[1], doEncode);
 
-  char *outData = nullptr;
+  if (data.size() == 0){
+    return 0;
+  }
+
+  std::vector<char> outData;
   size_t nGroup, nLeft, outSize;
-  {
-    nGroup = (size+2) / 3;
+  if(doEncode) {
+    nGroup = (data.size()+2) / 3;
     outSize = nGroup*4;
-    outData = new char[nGroup*4];
-    nLeft = size % 3;
+    outData = std::vector<char>(outSize);
+    nLeft = data.size() % 3;
+
+    for(int i = 0; 1 < nGroup - i; ++i){
+      encode(data.data() + 3*i, outData.data() + 4*i);
+    }
+    encodePadded(data.data() + 3*(nGroup-1), outData.data()+4*(nGroup-1), nLeft);
   }
-  for(int i = 0; i < nGroup - 1; ++i){
-    encode(indata + 3*i, outData + 4*i);
-  }
-  encodePadded(indata + 3*(nGroup-1), outData+4*(nGroup-1), nLeft);
-  for(int i = 0; i < outSize; ++i){
-    std::cout.put(outData[i]);
-    if((i+1) % 76 == 0){
-      std::cout.put(0x0D);
-      std::cout.put(0x0A);
+  else {
+    nGroup = data.size() / 4; 
+    outSize = nGroup * 3;
+    outData = std::vector<char>(outSize);
+    for(int i = 0; i < nGroup - 1; ++i){
+      decode(data.data() + 4*i, outData.data() + 3*i);
     }
   }
-  std::cout<<std::endl;
+  writeData(std::cout, outData, doEncode);
   return 0;
 }
